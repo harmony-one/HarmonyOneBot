a19aeae872e3b61e8f9eb9a69d333866c618883a
fegloff <fegloff@gmail.com>
2023-06-29 16:43:59 -0600

    minor changes

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,25 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default {
-  port: +(process.env.PORT || '3000'),
-  telegramBotAuthToken: process.env.TELEGRAM_BOT_AUTH_TOKEN || '',
+  port: +(process.env.PORT || "3000"),
+  telegramBotAuthToken: process.env.TELEGRAM_BOT_AUTH_TOKEN || "",
   openAiKey: process.env.OPENAI_API_KEY,
+  stableDiffusionHost: process.env.SD_HOST || "",
+  qr: {
+    checkReadable: false,
+  },
   imageGen: {
-    telegramFileUrl: process.env.TELEGRAM_FILE_URL || 'https://api.telegram.org/file/bot',
+    telegramFileUrl:
+      process.env.TELEGRAM_FILE_URL || "https://api.telegram.org/file/bot",
     completions: {
-      model: process.env.OPENAI_MODEL || 'text-davinci-003',
+      model: process.env.OPENAI_MODEL || "text-davinci-003",
       maxTokens: process.env.OPENAI_MAX_TOKENS || 140,
-      temperature: process.env.OPENAI_TEMPERATURE || 0.8
+      temperature: process.env.OPENAI_TEMPERATURE || 0.8,
     },
     sessionDefault: {
       numImages: 1,
-      imgSize: '1024x1024'
+      imgSize: "1024x1024",
     },
-  stableDiffusionHost: process.env.SD_HOST || '',
-  qr: {
-    checkReadable: false,
   }
-}
+};
