a0644aed7ec8af393753dca7aa3afc76f4280471
Yuriy <yuriy@harmony.one>
2023-07-01 02:21:44 +0300

    Fixed callback query parsing for SD images bot

--- src/modules/sd-images/utils.ts
+++ src/modules/sd-images/utils.ts
@@ -1,7 +1,9 @@
 import { randomBytes } from 'crypto'
 
+const rand = () => randomBytes(4).toString('hex');
+
 export const uuidv4 = () => {
-  return [randomBytes(4), randomBytes(4), randomBytes(4), randomBytes(4)].join('-');
+  return [1, 2, 3, 4].map(() => rand()).join('-');
 };
 
 export const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
