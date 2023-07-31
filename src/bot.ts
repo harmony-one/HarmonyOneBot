9f653df892576fa0cb97ae7bdd4327529971e34d
fegloff <fegloff@gmail.com>
2023-07-30 23:52:10 -0500

    add getEstimatedPrice logic + add ChatGpt and DalleGpt price calculation

--- src/bot.ts
+++ src/bot.ts
@@ -18,13 +18,14 @@ import { mainMenu } from "./pages";
 import { VoiceMemo } from "./modules/voice-memo";
 import { QRCodeBot } from "./modules/qrcode/QRCodeBot";
 import { SDImagesBot } from "./modules/sd-images";
-import { imageGen } from "./modules/open-ai/ImageGenBot";
+import { openAi } from "./modules/open-ai/openAiBot";
 import { oneCountry } from "./modules/1country/oneCountryBot";
 import { Wallet } from "./modules/wallet";
 import { WalletConnect } from "./modules/walletconnect";
 import { conversationHandler } from './modules/conversation-handler/conversationHandler'
 
 import config from "./config";
+import { ChatGPTModelsEnum } from "./modules/open-ai/types";
 
 
 const logger = pino({
@@ -115,7 +116,7 @@ bot.command("help", async (ctx) => {
 
 bot.use(conversationHandler)  
 bot.use(oneCountry);
-bot.use(imageGen);
+bot.use(openAi);
 
 bot.on("message", onMessage);
 bot.on("callback_query:data", onCallback);
---
+++ src/modules/open-ai/types.ts
@@ -0,0 +1,60 @@
+export interface ChatGPTModel {
+  name: string;
+  inputPrice: number;
+  outputPrice: number;
+  maxContextTokens: number;
+}
+
+export interface DalleGPTModel {
+  size: string;
+  price: number;
+}
+
+export enum ChatGPTModelsEnum {
+  GPT_4 = "gpt-4",
+  GPT_4_32K = "gpt-4-32k",
+  GPT_35_TURBO = "gpt-3.5-turbo",
+  GPT_35_TURBO_16K = "gpt-3.5-turbo-16k",
+}
+
+export const ChatGPTModels: Record<string, ChatGPTModel> = {
+  "gpt-4": {
+    name: "gpt-4",
+    inputPrice: 0.03,
+    outputPrice: 0.06,
+    maxContextTokens: 8000
+  },
+  "gpt-4-32k": {
+    name: "gpt-4-32k",
+    inputPrice: 0.06,
+    outputPrice: 0.12,
+    maxContextTokens: 32000
+  },
+  "gpt-3.5-turbo": {
+    name: "gpt-3.5-turbo",
+    inputPrice: 0.0015,
+    outputPrice: 0.002,
+    maxContextTokens: 4000
+  },
+  "gpt-3.5-turbo-16k": {
+    name: "gpt-3.5-turbo-16k",
+    inputPrice: 0.003,
+    outputPrice: 0.004,
+    maxContextTokens: 16000
+  },
+};
+
+export const DalleGPTModels: Record<string, DalleGPTModel> = {
+  "1024×1024": {
+    size: "1024×1024",
+    price: 0.020
+  },
+  "512x512": {
+    size: "512x512",
+    price: 0.018
+  },
+  "256x256": {
+    size: "256x256",
+    price: 0.016
+  },
+};
