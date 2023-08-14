96528949afba80447a1f36c3cb8baed6876eac77
fegloff <fegloff@gmail.com>
2023-08-14 13:11:52 -0500

    image menu hotfix

--- src/modules/sd-images/menu.ts
+++ src/modules/sd-images/menu.ts
@@ -2,27 +2,27 @@ import { Menu } from "@grammyjs/menu";
 import { BotContext } from "../types";
 import { MenuIds, menuText } from "../../constants";
 
-export const sdImagesMenuText = {
-  helpText: `🖼️ *Stable Diffusion Help*
+// export const sdImagesMenuText = {
+//   helpText: `🖼️ *Stable Diffusion Help*
 
-*1. GENERATE A SINGLE IMAGE*
-• Use */image <PROMPTS>*
-*Example:* 
-\`/image best quality, masterpiece, ultra high res, photorealistic, 1girl, offshoulder, smile\`
+// *1. GENERATE A SINGLE IMAGE*
+// • Use */image <PROMPTS>*
+// *Example:*
+// \`/image best quality, masterpiece, ultra high res, photorealistic, 1girl, offshoulder, smile\`
 
-*2. GENERAGE MULTIPLE IMAGES*
-• Use */images <PROMPTS>*
-*Example:* 
-\`/images best quality, masterpiece, ultra high res, photorealistic, 1girl, offshoulder, smile\`
+// *2. GENERAGE MULTIPLE IMAGES*
+// • Use */images <PROMPTS>*
+// *Example:*
+// \`/images best quality, masterpiece, ultra high res, photorealistic, 1girl, offshoulder, smile\`
 
-  `,
-};
+//   `,
+// };
 
 export const sdImagesMenu = new Menu<BotContext>(MenuIds.SD_IMAGES_MAIN).back(
-  menuText.imageMenu.backButton,
+  menuText.mainMenu.backButton,
   (ctx) => {
     ctx
-      .editMessageText(menuText.imageMenu.helpText, {
+      .editMessageText(menuText.mainMenu.menuName, {
         parse_mode: "Markdown",
         disable_web_page_preview: true,
       })
