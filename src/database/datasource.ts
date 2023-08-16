53c48439f2fe36a0a7d62f8d86f93d82d771596d
ahiipsa <ahiipsa@gmail.com>
2023-08-16 22:11:46 +0400

    Fixed database envs

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -5,11 +5,7 @@ import config from "../config"
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: config.db.host,
-  port: config.db.port,
-  username: config.db.username,
-  password: config.db.password,
-  database: config.db.database,
+  url: config.db.url,
   entities: [Credits],
   synchronize: false,
   migrations: ['./src/database/migrations/**/*.ts'],
