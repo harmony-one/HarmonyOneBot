e550d3f769ff529b3b5bd15b75eff188aac305c6
ahiipsa <ahiipsa@gmail.com>
2023-08-28 23:21:33 +0400

    Fixed app bootstrap

--- src/modules/schedule/index.ts
+++ src/modules/schedule/index.ts
@@ -92,20 +92,30 @@ export class BotSchedule {
   }
 
   private async runCronJob() {
-    cron.schedule('30 17 * * *', () => {
+    const task1 = cron.schedule('30 17 * * *', () => {
       this.prepareMetricsUpdate(true)
     }, {
       scheduled: true,
       timezone: "Europe/Lisbon"
     });
 
-    cron.schedule('00 18 * * *', () => {
+    const task2 = cron.schedule('00 18 * * *', () => {
       this.logger.info('Posting daily metrics')
       this.postMetricsUpdate()
     }, {
       scheduled: true,
       timezone: "Europe/Lisbon"
     });
+
+    const stopTasks = () => {
+      console.log('### stop tasks');
+
+      task1.stop();
+      task2.stop();
+    }
+
+    process.once("SIGINT", stopTasks)
+    process.once("SIGTERM", stopTasks);
 
     await this.prepareMetricsUpdate()
     // await this.postMetricsUpdate()
