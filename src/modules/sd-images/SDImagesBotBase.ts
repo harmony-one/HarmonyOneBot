2ededfba51590554e023c035f90aea05f1e816a7
Yuriy <yuriy@harmony.one>
2023-09-05 00:16:08 +0300

    SD Images: added logo cmd

--- src/modules/sd-images/SDImagesBotBase.ts
+++ src/modules/sd-images/SDImagesBotBase.ts
@@ -3,12 +3,14 @@ import { OnMessageContext, OnCallBackQueryData } from "../types";
 import { getTelegramFileUrl, loadFile, sleep, uuidv4 } from "./utils";
 import { InputFile } from "grammy";
 import { COMMAND } from './helpers';
+import { ILora } from "./api/loras-config";
 
 export interface ISession {
     id: string;
     author: string;
     prompt: string;
     model: IModel;
+    lora?: ILora;
     all_seeds?: string[];
     seed?: number;
     command: COMMAND;
@@ -30,12 +32,13 @@ export class SDImagesBotBase {
         params: {
             prompt: string;
             model: IModel;
+            lora?: ILora;
             command: COMMAND;
             all_seeds?: string[];
             seed?: string;
         }
     ) => {
-        const { prompt, model, command, all_seeds } = params;
+        const { prompt, model, command, all_seeds, lora } = params;
 
         const authorObj = await ctx.getAuthor();
         const author = `@${authorObj.user.username}`;
@@ -48,6 +51,7 @@ export class SDImagesBotBase {
             author,
             prompt: prompt,
             model,
+            lora,
             command,
             all_seeds,
             message
@@ -83,7 +87,7 @@ export class SDImagesBotBase {
         refundCallback: (reason?: string) => void,
         session: ISession
     ) => {
-        const { model, prompt, seed } = session;
+        const { model, prompt, seed, lora } = session;
         const uuid = uuidv4();
 
         try {
@@ -94,7 +98,8 @@ export class SDImagesBotBase {
             const imageBuffer = await this.sdNodeApi.generateImage({
                 prompt,
                 model,
-                seed
+                seed,
+                lora
             });
 
             const reqMessage = session.message ?
