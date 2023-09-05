ac0053fb8213002d4cabb5d9c20fe8baf04dd851
Yuriy <yuriy@harmony.one>
2023-09-05 16:09:18 +0300

    SD Images: fix

--- src/modules/sd-images/api/helpers.ts
+++ src/modules/sd-images/api/helpers.ts
@@ -123,7 +123,7 @@ export const getParamsFromPrompt = (originalPrompt: string, model: IModel): IPar
 
   if (loraMatch) {
     loraName = loraMatch[1];
-    loraStrength = Number(loraMatch[1]);
+    loraStrength = Number(loraMatch[2]);
     
     prompt = prompt.replace(/<lora:(.*):(.*)>/, '');
   }
