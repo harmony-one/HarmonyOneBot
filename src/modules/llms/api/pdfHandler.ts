60c329eaf6f1d852d6d628e6d715b17c14e57f51
fegloff <fegloff@gmail.com>
2023-09-28 16:54:46 -0500

    add pdf crawler logic for a demo

---
+++ src/modules/llms/api/pdfHandler.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosError } from 'axios'
+import config from '../../../config'
+import { type ChatConversation } from '../../types'
+
+export interface PdfCompletion {
+  completion: ChatConversation | undefined
+  prompt: string
+  price: number
+}
+
+export const handlePdf = async (prompt: string): Promise<PdfCompletion> => {
+  try {
+    const data = { question: prompt }
+    const url = `${config.llms.pdfUrl}/ask`
+    const response = await axios.post(url, data)
+    if (response) {
+      console.log(response.data)
+      return {
+        completion: {
+          content: response.data.response,
+          role: 'system'
+        },
+        prompt,
+        price: response.data.cost
+      }
+    }
+    return {
+      completion: undefined,
+      prompt,
+      price: 0
+    }
+  } catch (error: any) {
+    if (error instanceof AxiosError) {
+      console.log(error.code)
+      console.log(error.message)
+      console.log(error.stack)
+    }
+    throw error
+  }
+}
