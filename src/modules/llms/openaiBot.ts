154271545897e39907b6740289df02a047cd6e38
fegloff <fegloff@gmail.com>
2024-11-15 16:58:55 -0500

    add c0 command + update new command to set the current prompt to the default value

--- src/modules/llms/openaiBot.ts
+++ src/modules/llms/openaiBot.ts
@@ -49,9 +49,9 @@ export class OpenAIBot extends LlmsBase {
   ): boolean {
     const commands = ['last', ...this.supportedCommands]
     const hasCommand = ctx.hasCommand(commands)
-    if (ctx.hasCommand(SupportedCommands.new) && this.checkModel(ctx)) {
-      return true
-    }
+    // if (ctx.hasCommand(SupportedCommands.new) && this.checkModel(ctx)) {
+    //   return true
+    // }
     if (isMentioned(ctx)) {
       return true
     }
@@ -123,16 +123,16 @@ export class OpenAIBot extends LlmsBase {
       return
     }
 
-    if (
-      (ctx.hasCommand(SupportedCommands.new) ||
-      hasNewPrefix(ctx.message?.text ?? '') ||
-      (ctx.message?.text?.startsWith('new ') && ctx.chat?.type === 'private') && this.checkModel(ctx))
-    ) {
-      await this.onStop(ctx)
-      this.updateSessionModel(ctx, this.modelsEnum.GPT_4O)
-      await this.onChat(ctx, this.modelsEnum.GPT_4O, true, false)
-      return
-    }
+    // if (
+    //   (ctx.hasCommand(SupportedCommands.new) ||
+    //   hasNewPrefix(ctx.message?.text ?? '') ||
+    //   (ctx.message?.text?.startsWith('new ') && ctx.chat?.type === 'private') && this.checkModel(ctx))
+    // ) {
+    //   await this.onStop(ctx)
+    //   this.updateSessionModel(ctx, this.modelsEnum.GPT_4O)
+    //   await this.onChat(ctx, this.modelsEnum.GPT_4O, true, false)
+    //   return
+    // }
 
     const model = this.getModelFromContext(ctx)
     if (model) {
